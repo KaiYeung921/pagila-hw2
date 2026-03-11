72db642132ce0a4286c76c7a6bfa088ac7e8bcde
Kai <kyeung921@gmail.com>
2026-03-10 20:59:28 -0700

    solve problems

--- sql/05b.sql
+++ sql/05b.sql
@@ -3,3 +3,7 @@
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
 
+select a.last_name, a.first_name from actor as a
+LEFT JOIN customer as c on a.first_name = c.first_name and
+a.last_name = c.last_name
+where c.customer_id is null order by last_name, first_name;
